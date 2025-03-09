1695961dd48668fd3aa33cd7368ab9e8f2af63a1
Sayooj V <160754229+sayooj97@users.noreply.github.com>
2025-03-09 15:16:20 +0530

    Update setting.py

--- backend/setting.py
+++ backend/setting.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pc_builds_db',  # Replace with your database name
         'USER': 'sayooj',
-        'PASSWORD': '9895',
+        'PASSWORD': '',
         'HOST': 'localhost',  # Change if using a remote database
         'PORT': '3306',  # Default MySQL port
     }
